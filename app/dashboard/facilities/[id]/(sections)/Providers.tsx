c34c84a6196980deeaa51cec9a8dac4af439b845
salado-jofel <saladojofel@gmail.com>
2026-03-14 02:44:26 +0800

    fix: removed products in facility slug

--- app/dashboard/facilities/[id]/(sections)/Providers.tsx
+++ app/dashboard/facilities/[id]/(sections)/Providers.tsx
@@ -1,26 +1,22 @@
 "use client";
 
 import type { Facility } from "@/app/(interfaces)/facility";
-import type { Product } from "@/app/(interfaces)/product";
 import { useAppDispatch } from "@/store/hooks";
 import { type ReactNode, useEffect } from "react";
-import { setFacility, setProducts } from "../(redux)/facility-slice";
+import { setFacility } from "../(redux)/facility-slice";
 
 export default function Providers({
   children,
   facility,
-  products,
 }: {
   children: ReactNode;
   facility: Facility;
-  products: Product[];
 }) {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(setFacility(facility));
-    dispatch(setProducts(products));
-  }, [dispatch, facility, products]);
+  }, [dispatch, facility, ]);
 
   return <>{children}</>;
 }
