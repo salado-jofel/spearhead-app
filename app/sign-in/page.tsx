767019c2c64564c38006a96368ea85b302d7d9d6
salado-jofel <saladojofel@gmail.com>
2026-03-16 19:24:29 +0800

    fix: build error

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,10 +1,12 @@
+import { Suspense } from "react";
 import SignInForm from "./(sections)/SignInForm";
-
 
 export default function Page() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-slate-50 p-4">
-        <SignInForm/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <SignInForm />
+      </Suspense>
     </div>
-  )
+  );
 }
